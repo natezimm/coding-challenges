2fa17d6801bff88227518ee08d17f1a70e2734ca
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2018-11-17 22:21:19 -0500

    Create subsequence.js

---
+++ subsequence.js
@@ -0,0 +1,15 @@
+// Determine whether one string is part of another string in the same order
+
+function isSubsequence(str1, str2) {
+  var i = 0;
+  var j = 0;
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
+}
+
+isSubsequence("hello", "hello world");
