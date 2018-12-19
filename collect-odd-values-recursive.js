f974fa4a80c340eb981f3260a050ac732de3be91
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2018-12-18 21:28:52 -0500

    Create collect-odd-values-recursive.js

---
+++ collect-odd-values-recursive.js
@@ -0,0 +1,19 @@
+// Function that collects odd values in an array
+
+function collectOddValues(arr){
+    let newArr = [];
+    
+    if(arr.length === 0) {
+        return newArr;
+    }
+        
+    if(arr[0] % 2 !== 0){
+        newArr.push(arr[0]);
+    }
+        
+    newArr = newArr.concat(collectOddValues(arr.slice(1)));
+    return newArr;
+}
+
+collectOddValues([1,2,3,4,5]); //[1, 3, 5]
+                                   
