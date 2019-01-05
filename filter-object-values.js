3323fc8efef6c8a4e8d6c467705e759a48aa4c42
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2019-01-05 16:03:35 -0500

    Update filter-object-values.js

--- filter-object-values.js
+++ filter-object-values.js
@@ -1,7 +1,10 @@
-// Function searches array of objects and finds first value in array
+// Function searches array of objects and finds first value in array that matches the search parameter
 
 function findInObj(arr, key, searchValue){
     return arr.filter(function(val){
         return val[key] === searchValue;
     })[0];
 }
+
+find([1,2,3,4,5], 2) // 2
+find([1,2,3,4,5], 10) // undefined
