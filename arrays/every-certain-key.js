0f86ec1ae82cda839f11f991639bb91cb5de7929
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2019-02-16 21:22:17 -0500

    Create every-certain-key.js

---
+++ arrays/every-certain-key.js
@@ -0,0 +1,17 @@
+// Takes an array and returns true if every object has a certain key
+
+function hasCertainKey(arr, key){
+  return arr.every(function(val){
+    return key in val
+  })
+}
+
+    var arr = [
+        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
+        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
+        {title: "Instructor", first: 'Matt', last:"Lane"}, 
+        {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+    ]
+    
+    hasCertainKey(arr,'first') // true
+    hasCertainKey(arr,'isCatOwner') // false
