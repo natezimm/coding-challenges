16b384d42026e716083cb2dd819ea1ab336a533d
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2019-02-16 21:31:12 -0500

    Create every-certain-value.js

---
+++ arrays/every-certain-value.js
@@ -0,0 +1,17 @@
+// Takes an array and returns true if every object has that search value
+
+function hasCertainValue(arr, key, searchValue){
+  return arr.every(function(val){
+    return val[key] === searchValue;
+  })
+}
+
+    var arr = [
+        {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
+        {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
+        {title: "Instructor", first: 'Matt', last:"Lane"}, 
+        {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+    ]
+    
+    hasCertainValue(arr,'title','Instructor') // true
+    hasCertainValue(arr,'first','Elie') // false
