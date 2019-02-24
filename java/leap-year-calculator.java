6468af2de38d07a617c17262fccca2c753d1c944
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2019-02-24 13:54:47 -0500

    Create leap-year-calculator.java

---
+++ java/leap-year-calculator.java
@@ -0,0 +1,13 @@
+// This program calculates if a leap year is one or not 
+
+public class LeapYear {
+    public static boolean isLeapYear(int year){
+        if(year < 1 && year > 9999){
+            return false;
+        } else if((year % 4 == 0 && year % 100 != 0) && (year >= 1 && year <= 9999)|| year % 400 == 0 && (year >= 1 && year <= 9999)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
