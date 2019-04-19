9eacecb58944f60066eec2801becddad8c9021fb
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2019-04-18 20:45:21 -0400

    Create square-star.java

---
+++ java/square-star.java
@@ -0,0 +1,21 @@
+// Print square star of certain length
+
+public class DiagonalStar {
+    public static void printSquareStar(int number){
+        if(number < 5){
+            System.out.println("Invalid Value");
+        }else{
+            for (int i=1; i <= number; i++){
+    
+                for (int j=1; j <= number; j++){
+                    if( i == 1 || i == number || j == 1 || j == number || i == j || j == (number - i + 1)){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }  
+        }
+    }
+}
