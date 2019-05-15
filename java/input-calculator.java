3c5af6aca9133df634eea9efcdee9920d3076907
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2019-05-15 19:39:36 -0400

    Create input-calculator.java

---
+++ java/input-calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ 
+public class InputCalculator {
+public static void inputThenPrintSumAndAverage() {
+Scanner sc = new Scanner(System.in);
+int sum = 0;
+int count = 0;
+while (sc.hasNextInt()) {
+sum = sum + sc.nextInt();
+count++;
+}
+if (!sc.hasNextInt()) {
+long avg = Math.round((double)sum / (double)count);
+System.out.println("SUM = " + sum + " AVG = " + avg);
+}
+}
+} 
