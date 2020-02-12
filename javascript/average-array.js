c7c17d3d69eaa22a2e411c14da1aeae4c758c280
Nathan Zimmerman <nathanzimmerman411@gmail.com>
2020-02-11 21:48:39 -0500

    Create average-array.js

---
+++ javascript/average-array.js
@@ -0,0 +1,12 @@
+// Function to find average of an array
+
+function avg(arr) {
+	let total = 0;
+	//loop over each num
+	for (let num of arr) {
+		//add them together
+		total += num;
+	}
+	//divide by number of nums
+	return total / arr.length;
+}
